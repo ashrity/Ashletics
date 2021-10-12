2998f193466d922216f0aaa95b1fa25ec26ceea8
Ashrit Yerukola <ashrity@gmail.com>
2021-10-12 01:19:23 -0400

    Stylesheet Commits

--- components/styles.js
+++ components/styles.js
@@ -1,5 +1,5 @@
 import styled from 'styled-components';
-import {View, Text, Image} from 'react-native';
+import {View, Text, Image, TextInput, TouchableOpacity} from 'react-native';
 import Constants from 'expo-constants';
 
 const StatusBarHeight = Constants.statusBarHeight;
@@ -7,9 +7,9 @@ const StatusBarHeight = Constants.statusBarHeight;
 export const Colors = {
     primary: "#ffffff",
     secondary: "#E5E7EB",
-    tertiary: "#1F2937", 
+    tertiary: "#87A7BA", 
     darkLight: "#9CA3AF",
-    brand: "#6D28D9", 
+    brand: "#3FBCE7", 
     green: "#10B981",
     red: "#EF4444"
 };
@@ -52,4 +52,65 @@ export const SubTitle = styled.Text`
 
 export const StyledFormArea = styled.View`
     width: 90%;
+`
+
+export const StyleTextInput = styled.TextInput`
+    background-color: ${secondary};
+    padding: 15px; 
+    padding-left: 55px;
+    padding-right: 55px;
+    border-radius: 5px;
+    font-size: 16px;
+    height: 60px; 
+    margin-vertical: 3px;
+    margin-bottom: 10px;
+    color: ${tertiary};
+`;
+
+export const StyledInputLabel = styled.Text`
+    color: ${tertiary};
+    font-size: 13px; 
+    text-align: left;
+`
+
+export const LeftIcon = styled.View`
+    left: 15px;
+    top: 38px;
+    position: absolute;
+    z-index: 1;
+
+`
+
+export const RightIcon = styled.TouchableOpacity`
+    right: 15px;
+    top: 35px;
+    position: absolute;
+    z-index: 1;
+`;
+
+export const StyledButton = styled.TouchableOpacity`
+    padding: 15px;
+    background-color: ${brand};
+    justify-content: center;
+    align-items: center;
+    border-radius: 5px;
+    margin-vertical: 5px;
+    height: 60px;
+`
+
+export const ButtonText = styled.Text`
+    color: ${primary};
+    font-size: 16px;
+`
+
+export const ErrorBox = styled.Text`
+    text-align: center;
+    font-size: 13px;
+`
+
+export const Line = styled.View`
+    height: 1px;
+    width: 100%;
+    background-color: ${darkLight};
+    margin-vertical: 10px;
 `
